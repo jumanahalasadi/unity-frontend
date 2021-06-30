0c8ed555fd0a83f5e81b336a28b5ce76b8bcceac
Jumanah Al Asadi <jalasadi@deloitte.ca>
2021-06-30 17:25:06 -0400

    Fixing tiny bug

--- src/components/input-field/index.jsx
+++ src/components/input-field/index.jsx
@@ -4,10 +4,9 @@ import './styles.css';
 const Input = ({ name, label, register, errors, required, type }) => (
     <div className="form-control-input">
       <label htmlFor={name}>{label}{required && "*" }</label>
-      <input id={name} name={name} type={type} {...register(name, { required: true, maxLength: 100 })} />
+      <input id={name} name={name} type={type} {...register(name, { required: true, maxLength: 250 })} />
       {errors && errors[name]?.type === 'required' && <span className="error">This field is required</span>}
-      {errors && errors[name]?.type === 'minLength' && <span className="error">Please input at least 3 characters</span>}
-      {errors && errors[name]?.type === 'maxLength' && <span className="error">Max of 100 characters</span>}
+      {errors && errors[name]?.type === 'maxLength' && <span className="error">Max of 250 characters</span>}
       {errors && errors[name]?.type === 'pattern' && <span className="error">Must contain only letters</span>}
     </div>
 );
